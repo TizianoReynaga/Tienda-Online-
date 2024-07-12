2c9241ddd4f12548b37cb2e660b3732131794973
TizianoReynaga <reynaga.tiziano@tecnica7.edu.ar>
2024-07-12 12:33:25 -0300

    Corrección Navbar

--- src/navbar.js
+++ src/navbar.js
@@ -11,13 +11,13 @@ const navbar =
         <div class="collapse navbar-collapse " id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active text-white" aria-current="page" href="../index.html">Inicio</a>
+                    <a class="nav-link active text-white" aria-current="page" href="index.html">Inicio</a>
                 </li>
                 <li class="nav-item">
-                                <a class="nav-link active text-white" aria-current="page" href="../productos.html" >Productos</a>
+                                <a class="nav-link active text-white" aria-current="page" href="productos.html" >Productos</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link active text-white" aria-current="page" href="../contacto.html" >Contactanos</a>
+                    <a class="nav-link active text-white" aria-current="page" href="contacto.html" >Contactanos</a>
                 </li>
             </ul>
             <div>
